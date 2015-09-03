be97976d80d31f607d3a1217ac5c33727f6c4769
katiepeterso <katiepeterso@outlook.com>
2015-09-02 18:14:35 -0700

    initial commit

---
+++ MafiaMiniGames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MafiaMiniGames
+//
+//  Created by Katherine Peterson on 2015-09-02.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
